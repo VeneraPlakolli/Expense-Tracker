0dd5e0d740bf7920457e49780ce3c4acb17caf63
VeneraPlakolli <94639395+VeneraPlakolli@users.noreply.github.com>
2023-02-16 00:29:15 +0100

    check input validation

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -8,15 +8,28 @@ const ExpenseForm = (props) => {
     const [inputAmount, setInputAmount] = useState('');
     const [inputDate, setInputDate] = useState('');
 
+    const [isTitleValid, setTitleValid] = useState(true);
+    const [isAmountValid, setAmountValid] = useState(true);
+    const [isDateValid, setDateValid] = useState(true);
+
     const titleChangeHandler = (event) => {
+        if(event.target.value.trim().length > 0) {
+            setTitleValid(true);
+        }
         setInputTitle(event.target.value);
     };
 
     const amountChangeHandler = (event) => {
+        if(event.target.value.trim().length > 0) {
+            setAmountValid(true);
+        }
         setInputAmount(event.target.value);
     };
 
     const dateChangeHandler = (event) => {
+        if(event.target.value.trim().length > 0) {
+            setDateValid(true);
+        }
         setInputDate(event.target.value);
     };
 
@@ -29,6 +42,17 @@ const ExpenseForm = (props) => {
             date: new Date(inputDate)
         };
 
+        if(expenseData.title.length === 0) {
+            setTitleValid(false);
+        };
+        if(expenseData.amount.length === 0) {
+            setAmountValid(false);
+        };
+        if(inputDate.trim().length === 0) {
+            setDateValid(false);
+        };
+
+
         props.onSaveExpenseData(expenseData);
         setInputTitle('');
         setInputAmount('');
@@ -37,15 +61,15 @@ const ExpenseForm = (props) => {
 
     return <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
-            <div className="new-expense__control">
+            <div className={`new-expense__control control1 ${!isTitleValid ? 'invalidTitle' : ''}`}>
                 <label>Title</label>
                 <input type='text' value={inputTitle} onChange={titleChangeHandler} />
             </div>
-            <div className="new-expense__control">
+            <div className={`new-expense__control control2 ${!isAmountValid ? 'invalidAmount' : ''}`}>
                 <label>Amount</label>
                 <input type='number' value={inputAmount} onChange={amountChangeHandler} min="0.01" step="0.01" />
             </div>
-            <div className="new-expense__control">
+            <div className={`new-expense__control control3 ${!isDateValid ? 'invalidDate' : ''}`}>
                 <label>Date</label>
                 <input type='date' value={inputDate} onChange={dateChangeHandler} min="2019-01-01" max="2022-12-31" />
             </div>
